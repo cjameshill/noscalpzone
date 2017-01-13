31a13d49b88f3c4a92d0aed37f4a4f0da5516802
Chris Hill <hello@christopherjameshill.com>
2017-01-12 20:21:05 -0500

    Design: Menu bar, menu dropdown, and events list, and color-themes

--- resources/views/components/_primary-header.blade.php
+++ resources/views/components/_primary-header.blade.php
@@ -6,24 +6,50 @@
         </div>
 
         <div class="nav-center nav-main-menu">
-            <span class="nav-item">
-                <a href="" class="button is-medium has-transition">Music</a>
-                <a href="" class="button is-medium has-transition">Sport</a>
-                <a href="" class="button is-medium has-transition">Broadway</a>
-                <p class="control">
-                    <input type="text" class="input search search-main-menu has-transition" placeholder="Find your next event">
-                </p>
-            </span>
+           <span class="nav-item">
+               <span class="has-dropdown is-sport">
+                   <a href="" class="button is-secondary is-medium">Music</a>
+                   <div class="main-menu-dropdown">
+                       <div class="box">
+                           <header>
+                               <h1 class="title is-3">Music</h1>
+                           </header>
+                           <div class="columns is-fullheight is-multiline">
+                               <div class="column is-6">
+                                   <h1 class="title is-4">Events</h1>
+                                    @include('elements._event-item', ['colorTheme' => 'is-sport'])
+                                    @include('elements._event-item', ['colorTheme' => 'is-music'])
+                                    @include('elements._event-item', ['colorTheme' => 'is-theater'])
+
+                               </div>
+                               <div class="column is-6">
+                                   <h1 class="title is-4">Performers</h1>
+                                   <div class="columns is-multiline">
+                                       <div class="column is-3 performer-list-item is-music">
+                                           <div class="event-name">
+                                            <span>Coldplay</span>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+                           <footer>
+
+                           </footer>
+                       </div>
+                   </div>
+               </span>
+           </span>
         </div>
 
         <div class="nav-right nav-menu">
             <span class="nav-item">
                 @if(! Auth::check())
-                    <a href="/login" class="button is-medium has-transition">Login</a>
-                    <a href="/register" class="button is-medium has-transition is-primary">Sign Up</a>
+                    <a href="/login" class="button is-secondary is-medium">Login</a>
+                    <a href="/register" class="button is-medium is-primary">Sign Up</a>
                 @else
-                    <a href="/dashboard" class="button is-medium has-transition is-primary">Dashboard</a>
-                    <a href="/logout" class="button is-medium has-transition">Logout</a>
+                    <a href="/dashboard" class="button is-medium is-primary">Dashboard</a>
+                    <a href="/logout" class="button is-medium">Logout</a>
                 @endif
             </span>
         </div>
---
+++ resources/views/elements/_event-item.blade.php
@@ -0,0 +1,18 @@
+<div class="event-list-item {{ $colorTheme }}">
+    <div class="event-date">
+        <span class="date">Jan 4</span>
+        <span class="day">Fri</span>
+    </div>
+    <div class="event-name">
+        <span>Coldplay</span>
+    </div>
+    <div class="event-location">
+        <span class="icon"><i class="fa fa-map-marker"></i></span>
+        <span>Madison Square Garden</span>
+
+    </div>
+    <div class="event-button">
+        <button class="button buy-button">from $80</button>
+    </div>
+
+</div>
