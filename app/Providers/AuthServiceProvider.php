916eb64d7a0815a2e3581fbd602bc37bc74c8fcc
Chris Hill <hello@christopherjameshill.com>
2017-02-21 17:42:30 -0500

    Updated AuthService Provider - commented out register gate policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,11 +30,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        foreach($this->getPermissions() as $permission){
-            Gate::define($permission->name, function($user) use ($permission){
-                return $user->hasRole($permission->roles);
-            });
-        }
+//        foreach($this->getPermissions() as $permission){
+//            Gate::define($permission->name, function($user) use ($permission){
+//                return $user->hasRole($permission->roles);
+//            });
+//        }
 
     }
 
